402f660d93c552a97f940944035339f68a39c86b
Anastasiia <troyanovskaya2003@gmail.com>
2024-09-10 20:48:34 +0100

    project card-2

--- src/app/components/main-page/projects/projects.component.ts
+++ src/app/components/main-page/projects/projects.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ProjectComponent } from './project/project.component';
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
+  imports: [ ProjectComponent ]
 })
 export class ProjectsComponent {
   projects = [{title: 'Book Nook', subtitle: 'Book recommendation system', src: 'assets/projects/logo1.png'},];
